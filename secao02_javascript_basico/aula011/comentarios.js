1c7bf2210f0137364ca16ea18d5821aee081c39c
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-25 23:04:21 -0300

    aula011_comentario

---
+++ secao02_javascript_basico/aula011/comentarios.js
@@ -0,0 +1,19 @@
+// E escrever um comentario longo
+console.log("Olá Mundo!"); // Aqui tem outro comentario
+console.log("Linha 1");
+// Comentario
+/*
+Um bloco de codigo 
+Uma linha de codigo
+*/
+
+/**
+ * Um titulo
+ * 
+ * Loren ipsum
+ */
+console.log("Linha 2");
+// console.log("Linha 3");
+console.log("Linha 4");
+
+// Vai ser ignorado
