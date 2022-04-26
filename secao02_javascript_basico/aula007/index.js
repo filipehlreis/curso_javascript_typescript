83e29edfe802308c4ca8be36ce9ac7947bf286dc
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-25 21:41:57 -0300

    aula007_console.log

---
+++ secao02_javascript_basico/aula007/index.js
@@ -0,0 +1,13 @@
+console.log("Filipe 'Henrique'");
+console.log("Filipe Henrique");
+console.log('Filipe "Henrique"');
+console.log('Filipe Henrique');
+console.log(`"Filipe" 'Henrique'`);
+console.log(`Filipe Henrique`);
+
+// String = Texto
+
+console.log(123456, 123456.4684564);
+console.log(12, 654.458, 'Filipe Henrique de Novo');
+
+// Number = Numero
