8fcbfc0f9dcb6fd0bef11b25266a234434d76de7
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-26 22:42:32 -0300

    aula013_var_let

---
+++ secao02_javascript_basico/aula013/index.js
@@ -0,0 +1,41 @@
+let nome = 'João'; // String
+let nome2; // Somente declarou a variavel
+
+console.log(nome2);
+nome2 = 'Qualquer valor';// Inicializando a variavel
+console.log(nome2);
+console.log('');
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+
+// Não podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+let nomeCliente = 'Joao';
+console.log(nomeCliente);
+
+// Não pode começar o nome de uma variavel com um numero
+let nome1;
+let no1me;
+/* let 1nome; nao pode*/
+
+// Não podem conter espacos ou tracos
+/* let nome Cliente;
+let nome-Cliente; */
+
+/* Utilizamos camelCase */
+let nomeCompletoDoCliente = 'Filipe Henrique Leite dos Reis'
+console.log(nomeCompletoDoCliente)
+
+/* Case-sensitive */
+let nomeClienteBanco = 'Luiz'
+let nomeclientebanco = 'Otavio'
+console.log(nomeClienteBanco, nomeclientebanco)
+
+/* Não podemos redeclarar variaveis com let */
+
+/* NÃO UTILIZE VAR, UTILIZE LET */
