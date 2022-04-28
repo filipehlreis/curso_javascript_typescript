d82e8946771b03b5589af51d1f5c73630ba2c024
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-27 22:44:05 -0300

    aula018_var_let_diference

---
+++ secao02_javascript_basico/aula018/index.js
@@ -0,0 +1,8 @@
+// ECMAScript 2015 (ES6)
+sobrenome = 'Luiz'; // NãO faça isso#! pode gerar problemas futuros
+// SEMPRE CRIAR VARIAVEIS OU CONSTANTES USANDO let OU const
+
+var nome = 'Filipe';
+var nome = 'Henrique';
+
+console.log(nome)
