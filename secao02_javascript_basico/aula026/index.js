171b4353ff0902d6efdb0686b1040af1e4d1e983
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-29 21:41:38 -0300

    aula026 - mais sobre numbers

---
+++ secao02_javascript_basico/aula026/index.js
@@ -0,0 +1,46 @@
+/**
+ * Padrao IEEE 754-2008
+ */
+
+// let num1 = 1500;
+// let num1 = 10.578955465769;
+let num1 = 0.7;
+// let num1 = 10;
+// let num2 = 2.5;
+let num2 = 0.1;
+
+// num1 += num2;
+// num1 += num2;
+// num1 += num2; 
+
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+
+// num1 = num1.toFixed(2);
+// num1 = parseFloat(num1.toFixed(2));
+// num1 = Number (num1.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(typeof num1);
+
+
+// console.log(num1.toString(02)); // representacao binaria
+
+
+// console.log(num1.toFixed(3));
+
+// console.log(Number.isInteger(num1));
+// console.log(Number.isInteger(num2));
+
+// let temp = num1 * 'ola';
+// console.log(Number.isNaN(temp));
+// console.log(temp);
+
+// console.log(num1 + num2)
+// console.log(num1)
