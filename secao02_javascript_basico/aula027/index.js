98855507bd98f4dd78dc039ed56413daa1974e0d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-30 18:41:30 -0300

    aula027 - math

---
+++ secao02_javascript_basico/aula027/index.js
@@ -0,0 +1,19 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1);
+let num2 = Math.ceil(num1);
+
+
+// console.log(num2);
+console.log(Math.floor(num1));
+console.log(Math.ceil(num1));
+console.log(Math.round(num1));
+console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6));
+console.log(Math.random() * (10 - 5) + 5);
+console.log(Math.round(Math.random() * (10 - 5) + 5));
+console.log(Math.round(Math.random(5, 10) * (10 - 5) + 5));
+console.log(Math.random(5, 10));
+console.log(Math.pow(2, 10));
+
+
+console.log(10 ** (1 / 2));
