6f7fced8a3da17dedc0ee18538df177e5ef4f009
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-05-03 22:30:12 -0300

    aula037 - operadores de comparacao

---
+++ secao03_logica_de_programacao/aula037/index.js
@@ -0,0 +1,26 @@
+/**
+ * Operadores de comparacao
+
+>   maior que
+<   menor que
+>=  maior que ou igual a
+<=  menor que ou igual a
+==  igualdade (valor) ************ nao utilizado
+=== igualdade estrita (valor e tipo)
+!=  diferente  ************ nao utilizado
+!== diferente estrita
+
+ */
+
+const num1 = 10;
+const num2 = '5';
+const comp = 10 > 5;
+const comp2 = num1 >= num2;
+const comp3 = 12 <= 11;
+const comp4 = 12 == 11;
+
+
+console.log(comp);
+console.log(comp2);
+console.log(comp3);
+console.log(comp4);
