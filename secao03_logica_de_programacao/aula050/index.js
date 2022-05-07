0e5775d0d90bf0d2584d578483fa78892cfca7f3
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-05-07 18:30:12 -0300

    aula050 - diferenca entre var e let

---
+++ secao03_logica_de_programacao/aula050/index.js
@@ -0,0 +1,84 @@
+const verdadeira = true;
+
+/**
+ * LET tem escopo de bloco { ... bloco }
+ * VAR só tem escopo de função
+ */
+
+
+/*
+let nome = 'Luiz';
+var nome2 = 'Luiz';
+
+
+if (verdadeira) {
+    console.log(nome, nome2);
+}
+
+if (verdadeira) {
+    let nome = 'Otavio';
+    console.log(nome, nome2);
+
+    if (verdadeira) {
+        let nome = 'Outra coisa'
+        console.log('OK', nome, nome2);
+    }
+
+    console.log(nome, nome2);
+
+}
+
+*/
+
+
+
+
+/*
+let nome = 'Luiz'; // criando
+var nome2 = 'Luiz';
+
+if (verdadeira) {
+    let nome = 'Otavio';
+    var nome2 = 'Rogerio'
+    console.log(nome, nome2);
+
+    if (verdadeira) {
+        let nome = 'Outra coisa'
+        var nome2 = 'Ronaldo'
+        console.log(nome, nome2);
+    }
+    console.log(nome, nome2);
+
+    // console.log(nome, nome2);
+
+}
+console.log(nome, nome2);
+
+*/
+
+
+
+/*
+function falaOi() {
+
+    if (verdadeira) {
+        let nome = 'Luiz';
+        var sobrenome = 'Miranda';
+    }
+    console.log(sobrenome);
+}
+
+falaOi();
+*/
+
+
+
+
+console.log(sobrenome)
+console.log(nome)
+
+
+
+
+let nome = 'Otavio'
+var sobrenome = 'Miranda';
