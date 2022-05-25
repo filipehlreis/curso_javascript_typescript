39116007b2205aa35a6dc071b8f58be09c0b05e1
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-05-24 21:36:41 -0300

    aula091 - getter e setters

---
+++ secao06_javascript_avancado_objetos_prototypes/aula091/index.js
@@ -0,0 +1,52 @@
+/**
+ * defineProperty - Getter e Setters
+ */
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    let estoquePrivado = estoque;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        configurable: true, // configuravel
+        get: function () {
+            return estoquePrivado;
+        },
+        set: function (valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('Messagem');
+            }
+
+            estoquePrivado = valor;
+        },
+
+    });
+}
+
+
+function criaProduto(nome) {
+    return {
+        get nome() {
+            return nome;
+        },
+        set nome(valor) {
+            valor = valor.replace('coisa.', '');
+            nome = valor;
+        }
+    };
+}
+
+
+
+// const p1 = new Produto('Camiseta', 20, 3);
+// console.log(p1);
+
+// p1.estoque = 'fsafsd3fsd';
+// console.log(p1.estoque);
+
+
+const p2 = criaProduto('Camiseta');
+p2.nome = 'Qualquer coisa.';
+console.log(p2.nome);
