62ceadcb1759ef5981334def4b7c16c28205e073
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-05-25 22:23:55 -0300

    aula094 - manipulando prototypes
    Aula explicativa sobre prototypes, como criar, inserir cadeias de prototype, associar diversos objetos a prototypes específicos, dentre outras coisas.

---
+++ secao06_javascript_avancado_objetos_prototypes/aula094/index.js
@@ -0,0 +1,96 @@
+// new Object -> Object.prototype
+const objA = {
+    chaveA: 'A',
+    // __proto__: Object.prototype
+};
+
+// new Object -> Object.prototype
+const objB = {
+    chaveB: 'B',
+    // __proto__: Object.prototype
+};
+
+const objC = new Object();
+objC.chaveC = 'C';
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+
+console.log(objC.chaveA);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+
+}
+
+Produto.prototype.desconto = function (percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+}
+
+
+Produto.prototype.aumenta = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+
+const p1 = new Produto('Camiseta', 50);
+p1.desconto(10);
+
+
+// literal
+const p2 = {
+    nome: 'Caneca',
+    preco: 15,
+}
+
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumenta(10);
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 113,
+    },
+    tamanho2: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42,
+    },
+});
+
+p3.aumenta(10);
+console.log(p3);
+
+
+console.log(p1);
+console.log(p2);
+
+
+
+
+
+
+
+
+
+
