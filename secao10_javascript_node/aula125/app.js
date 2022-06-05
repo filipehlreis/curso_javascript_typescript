c9f9d4c0625fd00143a6c3bcd6d725345e3fbdb4
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-06-05 19:58:03 -0300

    aula125 - modules pt2
    Nesta segunda parte da aula, o professor demonstra mais alguns detalhes sobre módulos, e principalmente como chamar módulos de qualquer outra pasta, usando caminho relativo, e demonstrou também um pouco sobre como resolver caminhos utilizando o modulo Path, já presente no JS.

---
+++ secao10_javascript_node/aula125/app.js
@@ -0,0 +1,45 @@
+// const multiplicacao = require('./mod');
+// console.log(multiplicacao(2, 2));
+
+
+// const n = require('./mod');
+// console.log(n);
+
+
+
+// const Cachorro = require('./B/C/D/mod');
+
+// const cachorrinho = new Cachorro('Dog');
+// cachorrinho.latir();
+
+
+
+
+
+// const Cachorro = require('./Z/mod2');
+
+// // console.log(Cachorro);
+// const c1 = new Cachorro('Dogi');
+// c1.latir();
+
+
+// const path = require('path');
+
+// // console.log(__filename);
+// // console.log(__dirname);
+
+// console.log(path.resolve(__dirname,));
+// console.log(path.resolve(__dirname, '..'));
+// console.log(path.resolve(__dirname, '..', '..'));
+// console.log(path.resolve(__dirname, '..', '..', '..',));
+// console.log(path.resolve(__dirname, '..', '..', '..', '..',));
+// console.log(path.resolve(__dirname, '..', '..', '..', '..', '..',));
+
+// console.log();
+// console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
+
+
+const Cachorro = require('./Z/mod2.js');
+
+const c1 = new Cachorro('Dog');
+c1.latir();
