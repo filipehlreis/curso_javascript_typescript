e2efe3bbe5441a19888c062d24fa95e8d8c8bb69
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-06-21 22:09:27 -0300

    aula181 - rotas de aluno
    Nesta aula o professor criou uma rota de aluno pra mostrar todos os alunos cadastrados no banco de dados, e também corrigiu um campo de email para deixa-lo como campo único, não permitir que 2+ alunos tenham o mesmo email.

--- secao13_javascript_api_rest/api_rest/src/controllers/HomeController.js
+++ secao13_javascript_api_rest/api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Maria',
-      sobrenome: 'Angelita',
-      email: 'maria@email.com',
-      idade: 23,
-      peso: 55.3,
-      altura: 1.63,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
