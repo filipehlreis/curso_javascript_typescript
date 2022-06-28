37603282732d72732f83a90000759fa69d1ddcb0
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-06-27 22:04:28 -0300

    aula193 - novas configuracoes do ESLint e Prettier
    Nesta aula foi passada todas as configurações necessárias para deixar atualizado o ESLint e o Prettier.

--- secao14_javascript_react/react_cursoJS/lista_tarefas/.eslintrc.js
+++ secao14_javascript_react/react_cursoJS/lista_tarefas/.eslintrc.js
@@ -2,12 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,10 +23,13 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 'off',
   },
 };
