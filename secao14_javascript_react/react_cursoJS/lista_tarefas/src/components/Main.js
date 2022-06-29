edb91dbd5685d8a4a5378f67e12e3e40bbca3a9f
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-06-28 21:56:42 -0300

    aula196- estilizando as tarefas
    Nesta aula foi desenvolvida uma estilização simples para tarefas fictícias.

--- secao14_javascript_react/react_cursoJS/lista_tarefas/src/components/Main.js
+++ secao14_javascript_react/react_cursoJS/lista_tarefas/src/components/Main.js
@@ -3,11 +3,19 @@ import React, { Component } from 'react';
 // Form
 import { FaPlus } from 'react-icons/fa';
 
+// Tarefas
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [
+      'Fazer café',
+      'Beber agua',
+      'Estudar',
+    ],
   };
 
   handleChange = (e) => {
@@ -17,7 +25,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className='main'>
@@ -32,6 +40,19 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className='tarefas'>
+          {tarefas.map(tarefa => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className='edit' />
+                <FaWindowClose className='delete' />
+              </div>
+            </li>
+          ))}
+        </ul>
+
       </div>
     );
   }
