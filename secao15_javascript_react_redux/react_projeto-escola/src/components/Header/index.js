e3f41b08a6e33888a323880c9e1d28a4740d137a
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-04 22:10:03 -0300

    aula208 - react router DOM
    Nesta aula o professor configurou algumas rotas utilizando React Router DOM, e entre o momento em que foi gravado a aula e eu estar fazendo hoje, houve atualização e foi necessário pesquisar para poder corrigir.
    Questão do Switch para Routes, e component para element.

--- secao15_javascript_react_redux/react_projeto-escola/src/components/Header/index.js
+++ secao15_javascript_react_redux/react_projeto-escola/src/components/Header/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import { Nav } from './styled';
 
 export default function Header() {
   return (
-    <>
-      <Nav>
-        <a href="localhost:3000">
-          <FaHome size={24} />
-        </a>
-        <a href="localhost:3000">
-          <FaUserAlt size={24} />
-        </a>
-        <a href="localhost:3000">
-          <FaSignInAlt size={24} />
-        </a>
-      </Nav>
-      <h1>Oi</h1>
-    </>
+    <Nav>
+      <Link to="/">
+        <FaHome size={24} />
+      </Link>
+      <Link to="/login">
+        <FaUserAlt size={24} />
+      </Link>
+      <Link to="/qualquercoisa">
+        <FaSignInAlt size={24} />
+      </Link>
+    </Nav>
   );
 }
