1890beaeb7af80e2d47d3bec1064642c68341e7e
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-10 22:12:36 -0300

    aula223 - login pt2/2
    Nesta segunda parte da aula, o professor deu continuidade para criar a requisição para logar o usuário, fez os esquemas de haver e permanecer o Authorization nos headers, e eu encontrei uma falha em escrita, na qual no arquivo do reducer estava escrito isLoggedId ao invés de isLoggedIn, e isto ocasionava uma falha de acessar rotas fechadas.

--- secao15_javascript_react_redux/react_projeto-escola/src/routes/MyRoute.js
+++ secao15_javascript_react_redux/react_projeto-escola/src/routes/MyRoute.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 export default function MyRoute({ component: Component, isClosed, ...rest }) {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  console.log(isLoggedIn);
+  console.log(isClosed);
 
   if (isClosed && !isLoggedIn) {
     return (
--- secao15_javascript_react_redux/react_projeto-escola/src/store/modules/auth/reducer.js
+++ secao15_javascript_react_redux/react_projeto-escola/src/store/modules/auth/reducer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import * as types from '../types';
 
 const initialState = {
-  isLoggedId: false,
+  isLoggedIn: false,
   token: false,
   user: {},
   isLoading: false,
@@ -11,9 +11,17 @@ const initialState = {
 // eslint-disable-next-line default-param-last
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case types.LOGIN_REQUEST: {
-      console.log('REDUCER', action.payload);
-      return state;
+    case types.LOGIN_SUCCESS: {
+      const newState = { ...state };
+      newState.isLoggedIn = true;
+      newState.token = action.payload.token;
+      newState.user = action.payload.user;
+
+      return newState;
+    }
+    case types.LOGIN_FAILURE: {
+      const newState = { ...initialState };
+      return newState;
     }
     default: {
       return state;
