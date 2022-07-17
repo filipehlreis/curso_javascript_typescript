5e0790812f3830cd060b63aa95c6c3224fefc807
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-17 20:14:07 -0300

    aula246 - lookahead e lookbehind - lookaround

--- secao16_javascript_expressoes_regulares/base.js
+++ secao16_javascript_expressoes_regulares/base.js
@@ -58,7 +58,15 @@ Os Ips são:
    255.255.255.255
 `;
 
+const lookahead = `
+ONLINE  192.168.0.1 ABCDEF inactive
+OFFLINE  192.168.0.2 ABCDEF active
+ONLINE  192.168.0.3 ABCDEF active
+ONLINE  192.168.0.4 ABCDEF active
+OFFLINE  192.168.0.5 ABCDEF active
+OFFLINE  192.168.0.6 ABCDEF inactive
+`;
 
 module.exports = {
-  texto, arquivos, html, alfabeto_texto, cpfs, cpfs2, ips, html2
+  texto, arquivos, html, alfabeto_texto, cpfs, cpfs2, ips, html2, lookahead
 }
