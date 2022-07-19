36e804928c9fc2df10d3b193598a980b7e1aceac
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-18 22:48:12 -0300

    aula257 - type annotation
    Nesta aula o professor explicou e exemplificou sobre type annotation em Typescript.

---
+++ secao18_typescript_basico/src/A0001-type-annotation/index.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+// tipos basicos
+let nome: string = 'Luiz'; // qualquer tipo de strings: '' "" ``
+let idade: number = 30; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
+let adulto: boolean = true; // true ou false
+let simbolo: symbol = Symbol('qualquer-symbol'); // symbol
+// let big: bigint = 10n; // bigint
+
+// Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+let arrayDeNumeros2: number[] = [1, 2, 3];
+let arrayDeStrings: Array<string> = ['a', 'b'];
+let arrayDeStrings2: string[] = ['a', 'b'];
+
+// Objetos
+let pessoa: { nome: string; idade: number; adulto?: boolean } = {
+  idade: 30,
+  nome: 'Filipe',
+};
+
+// Funcoes
+function soma(x: number, y: number): number {
+  return x + y;
+}
+const result = soma(2, 2);
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
