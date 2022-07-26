eb5b569246020ce4d6da47430b42b76c705ca9b0
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-25 21:12:22 -0300

    aula294 - implementando-tipos

---
+++ secao18_typescript_basico/src/A0033-implementado-tipos/index.ts
@@ -0,0 +1,20 @@
+type TipoNome = {
+  nome: string;
+};
+type TipoSobrenome = {
+  sobrenome: string;
+};
+type TipoNomeCompleto = {
+  nomeCompleto: () => string;
+};
+
+export class Pessoa implements TipoNome, TipoSobrenome, TipoNomeCompleto {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Filipe', 'Reis');
+console.log(pessoa.nomeCompleto());
