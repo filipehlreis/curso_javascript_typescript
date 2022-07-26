e75d0deee002a951b449d155c0ba6714dccdf6ca
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-25 23:03:21 -0300

    aula298 - type guard

---
+++ secao18_typescript_basico/src/A0037-type-guards/index.ts
@@ -0,0 +1,31 @@
+export function add(a: unknown, b: unknown) {
+  return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}`;
+}
+
+console.log(add(1, 5));
+console.log(add('a', 5));
+
+type Pessoa = { tipo: 'pessoa'; nome: string };
+type Animal = { tipo: 'animal'; cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  tipo: 'pessoa' = 'pessoa';
+  constructor(public nome: string) {}
+}
+
+function mostraNome(obj: PessoaOuAnimal): void {
+  // if ('nome' in obj) console.log(obj.nome);
+  // if (obj instanceof Aluno) console.log(obj.nome);
+  switch (obj.tipo) {
+    case 'pessoa':
+      console.log(obj.nome);
+      break;
+    case 'animal':
+      console.log('Isso é um animal', obj.cor);
+      break;
+  }
+}
+
+mostraNome(new Aluno('joao'));
+mostraNome({ tipo: 'animal', cor: 'Rosa' });
