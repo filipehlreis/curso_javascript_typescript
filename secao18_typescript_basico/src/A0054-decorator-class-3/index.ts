f6263236731087b00289af34b2a087389c7bc25e
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-29 19:16:35 -0300

    aula316 - decorator factories

---
+++ secao18_typescript_basico/src/A0054-decorator-class-3/index.ts
@@ -0,0 +1,32 @@
+function inverteNomeECor(param1: string, param2: string) {
+  // Closure
+  return function <T extends new (...args: any[]) => any>(target: T): T {
+    console.log('Sou o decorador e recebi', target);
+
+    return class extends target {
+      cor: string;
+
+      constructor(...args: any[]) {
+        super(...args);
+        // this.cor = 'Qualquer coisa';
+        this.nome = this.inverte(args[0]);
+        this.cor = this.inverte(args[1]);
+      }
+
+      inverte(valor: string): string {
+        return valor.split('').reverse().join('') + ' ' + param1 + ' ' + param2;
+      }
+    };
+  };
+}
+
+@inverteNomeECor('Valor 1', 'Valor 2')
+export class Animal {
+  constructor(public nome: string, public cor: string) {
+    console.log('Sou a classe');
+  }
+}
+
+// //
+const animal = new Animal('Filipe', 'roxo');
+console.log(animal);
