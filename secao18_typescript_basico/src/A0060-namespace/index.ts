a8bfc0f99ec2259f2c5687de016e7f36047a702c
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-30 17:25:32 -0300

    aula322 - criando um namespace

---
+++ secao18_typescript_basico/src/A0060-namespace/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace MeuNamespace {
+  export const nomeDoNamespace = 'Filipe';
+
+  export class PessoaDoNamespace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoaDoNamespace = new PessoaDoNamespace('Filipe');
+  console.log(pessoaDoNamespace);
+
+  export namespace OutroNamespace {
+    export const nomeDoNamespace = 'Filipe 2';
+  }
+}
+
+const pessoaDoNamespace = new MeuNamespace.PessoaDoNamespace('Filipe');
+console.log(pessoaDoNamespace);
+console.log(MeuNamespace.nomeDoNamespace);
+console.log(MeuNamespace.OutroNamespace.nomeDoNamespace);
+
+export default 1;
