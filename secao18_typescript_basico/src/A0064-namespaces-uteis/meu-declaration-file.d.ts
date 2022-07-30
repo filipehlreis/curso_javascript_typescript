744908eb1d08ffc2a2165b9b1d48752dc65c4295
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-07-30 19:59:29 -0300

    aula327 - estendendo tipos de bibliotecas de terceiros

---
+++ secao18_typescript_basico/src/A0064-namespaces-uteis/meu-declaration-file.d.ts
@@ -0,0 +1,11 @@
+declare namespace _ {
+  declare interface LoDashStatic {
+    mul(array: number[]): number;
+  }
+}
+
+declare namespace NodeJS {
+  declare interface Global {
+    MINHAGLOBAL: string;
+  }
+}
