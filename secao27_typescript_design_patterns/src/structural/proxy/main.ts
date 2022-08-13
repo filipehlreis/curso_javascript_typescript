2a96b4c6d3e871f76eb1b96fb8dedb9e10661e3f
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-08-13 20:16:16 -0300

    aula405 - design patterns - proxy

---
+++ secao27_typescript_design_patterns/src/structural/proxy/main.ts
@@ -0,0 +1,15 @@
+import { SystemUserProxy } from './system-user/system-user-proxy';
+
+async function clientCode(): Promise<void> {
+  const user = new SystemUserProxy('Filipe', 'filipereis');
+  console.log('Isso vai levar 2 segundos.');
+  console.log(await user.getAddresses());
+
+  console.log('Isso vai se repetir.(CACHE)');
+
+  for (let i = 0; i < 5; i++) {
+    console.log(await user.getAddresses());
+  }
+}
+
+clientCode();
