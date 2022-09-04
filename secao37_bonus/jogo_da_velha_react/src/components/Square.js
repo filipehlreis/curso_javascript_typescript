d3044e7e801aec9fc21c43299f1c3da9c949159d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-04 15:30:41 -0300

    aula642 - jogo da velha - React Hooks e React Context

---
+++ secao37_bonus/jogo_da_velha_react/src/components/Square.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { GameContext } from "../contexts/GameContext";
+
+import t from 'prop-types';
+
+export default function Square({ value, index }) {
+  const {
+    squares,
+    setSquares,
+    isXNext,
+    setIsXNext,
+    whoIsWinner,
+    history,
+    setHistory,
+  } = useContext(GameContext);
+
+  function handleClick() {
+    if (squares[index]) return;
+    if (whoIsWinner) return;
+
+    const newSquares = [...squares];
+    newSquares[index] = isXNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+
+    setHistory([...history, {
+      squares: [...squares],
+      isNext: isXNext,
+      whoIsWinner,
+    }])
+  }
+
+
+  return <button type="button" onClick={handleClick} >{value}</button>;
+}
+
+Square.defaultProps = {
+  value: null,
+}
+
+Square.propTypes = {
+  value: t.string,
+  index: t.number.isRequired,
+}
